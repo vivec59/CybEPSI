163cec499daf92828f9e462fa6455b7233f79c4a
BettyFava <florence.delbarre@epsi.fr>
2015-04-27 15:46:21 +0200

    Commit initial avec Spring BOOT

---
+++ ArchitectureSOA/src/main/java/Util/CybEPSI.java
@@ -0,0 +1,13 @@
+package Util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class CybEPSI{
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(CybEPSI.class, args);
+    }
+}
